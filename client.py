e48b39a1acfb04528220c550b3d1549d53752475
Artem <220220220220@rambler.ru>
2021-05-17 13:21:50 +0300

    Исправление на замечания к ДЗ 3. Отправку сообщений напишу позже (на работе полный АХТУНГ)

--- client.py
+++ client.py
@@ -5,26 +5,31 @@ import log.client_log_config
 
 logger = logging.getLogger('client')
 
-logger.debug('Start client successfully')
-try:
 
-    logger.debug('Start client successfully')
+def client():
+    try:
 
-    s = socket(AF_INET, SOCK_STREAM)
-    s.connect(('localhost', 7777))
-    msg = {
-        "action": "authenticate",
-        "time": "<unix timestamp>",
-        "user": {
-            "account_name": "WildCate",
-            "password": "Think"
+        logger.debug('Start client successfully')
+
+        s = socket(AF_INET, SOCK_STREAM)
+        s.connect(('localhost', 7777))
+        msg = {
+            "action": "authenticate",
+            "time": "<unix timestamp>",
+            "user": {
+                "account_name": "WildCate",
+                "password": "Think"
+            }
         }
-    }
-    s.send(pickle.dumps(msg))
-    data = s.recv(1024)
-    msg_server = pickle.loads(data)
-    logger.debug("Message from server received")
-    s.close()
-    logger.debug('App client ending')
-except:
-    logger.critical('Boss, ull disappeared!!!')
+        s.send(pickle.dumps(msg))
+        data = s.recv(1024)
+        msg_server = pickle.loads(data)
+        logger.debug(f"Message from server received: {msg_server}")
+        s.close()
+        logger.debug('App client ending')
+    except:
+        logger.critical('Boss, ull disappeared!!!')
+
+
+if __name__ == '__main__':
+    client()
--- server.py
+++ server.py
@@ -1,33 +1,37 @@
 import logging
-from socket import *
+from socket import SOCK_STREAM, AF_INET, socket
 import pickle
 import log.server_log_config
 
-
 logger = logging.getLogger('server')
 
-try:
-    s = socket(AF_INET, SOCK_STREAM)
-    s.bind(('', 7777))
-    s.listen(5)
+def server():
+    try:
+        s = socket(AF_INET, SOCK_STREAM)
+        s.bind(('', 7777))
+        s.listen(5)
 
-    while True:
-        logger.debug('Start server successfully')
-        client, addr = s.accept()
-        data = client.recv(1024)
-        response = {
-            'response': 200,
-            'alert': 'Не очень нужное сообщение'
-        }
-        client.send(pickle.dumps(response))
-        client.close()
-        logger.debug('App server ending')
-        logger.debug('Save in data start')
-        with open('tests/data.txt', 'w') as dat:
-            dat.write(str(s) + '\n')
-            dat.write(str(pickle.loads(data)) + '\n')
-            dat.write(str(response) + '\n')
-        logger.debug('Save in data end')
+        while True:
+            logger.debug('Start server successfully')
+            client, addr = s.accept()
+            data = client.recv(1024)
+            response = {
+                'response': 200,
+                'alert': 'Не очень нужное сообщение'
+            }
+            client.send(pickle.dumps(response))
+            client.close()
+            logger.debug('App server ending')
+            logger.debug('Save in data start')
+            with open('tests/data.txt', 'w') as dat:
+                dat.write(str(s) + '\n')
+                dat.write(str(pickle.loads(data)) + '\n')
+                dat.write(str(response) + '\n')
+            logger.debug('Save in data end')
 
-except:
-    logger.critical('Boss, ull disappeared!!!')
+    except:
+        logger.critical('Boss, ull disappeared!!!')
+
+
+if __name__ == '__main__':
+    server()
