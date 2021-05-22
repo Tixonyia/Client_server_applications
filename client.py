9bdadf272b99f2daf872b48fcc538c5ca0bc3fe2
Artem <220220220220@rambler.ru>
2021-05-22 17:51:12 +0300

    Всё по-переделал.

--- client.py
+++ client.py
@@ -1,18 +1,69 @@
-from socket import *
-import pickle
+import logging
+import select
+import chat.chat as chat
+import chat.base as base
 
-s = socket(AF_INET, SOCK_STREAM)
-s.connect(('localhost', 7777))
-msg = {
-    "action": "authenticate",
-    "time": "<unix timestamp>",
-    "user": {
-        "account_name": "WildCate",
-        "password": "Think"
-    }
-}
-s.send(pickle.dumps(msg))
-data = s.recv(1024)
-msg_server = pickle.loads(data)
-print(f"Сообщение от сервера: {msg_server}")
-s.close()
+logger = logging.getLogger('chat.client')
+
+
+def main():
+    logger.debug('App started')
+
+    parser = chat.create_parser()
+    namespace = parser.parse_args()
+
+    client_name = input('Введите имя: ')
+
+    sock = chat.get_client_socket(namespace.addr, namespace.port)
+    serv_addr = sock.getpeername()
+    start_info = f'Connected to server: {serv_addr[0]}:{serv_addr[1]}'
+    print(start_info)
+    logger.info(start_info)
+
+    base.PRESENCE['user']['account_name'] = client_name
+    try:
+        chat.send_data(sock, base.PRESENCE)
+    except ConnectionResetError as e:
+        logger.error(e)
+        sock.close()
+        exit(1)
+
+    while True:
+        r = []
+
+        try:
+            r, w, e = select.select([sock], [], [], 1)
+        except Exception as e:
+            pass
+
+        if sock in r:
+            try:
+                data = chat.get_data(sock)
+            except ConnectionResetError as e:
+                logger.error(e)
+                break
+
+            if data['response'] != '200':
+                logger.debug('App ending')
+                break
+
+            if 'messages' in data:
+                for message in data['messages']:
+                    print(f'{message["time"]} - {message["from"]}: {message["message"]}')
+
+        else:
+            msg = input('Введите сообщение ("exit" для выхода): ')
+            if msg:
+                base.MESSAGE['message'] = msg
+
+                try:
+                    chat.send_data(sock, base.MESSAGE)
+                except ConnectionResetError as e:
+                    logger.error(e)
+                    break
+
+    sock.close()
+
+
+if __name__ == '__main__':
+    main()
